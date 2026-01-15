0ca7f29850cb6929dc3bbfd253b5cf1f45b1486b
mashmllo <52559284+mashmllo@users.noreply.github.com>
2026-01-15 22:30:42 +0800

    Add Task class for task list
    Introduce a Task class to encapsulate task data and behavior.
    
    Each task has a name and a completion status, with methods to mark the task as done or not done. A toString() method is also provided to display the task with its status icon.
    
    This improves code organization and makes it easier to extend task functionality in the future.

---
+++ src/main/java/Task.java
@@ -0,0 +1,48 @@
+/**
+ * A task object to encapsulate task data
+ * <p>
+ * Each task has a name and a status to indicate whether it is done.
+ */
+public class Task {
+
+    private final String name;
+    private boolean isDone;
+
+    /**
+     * Constructor of the new task
+     * @param name name of the task
+     */
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    /**
+     * Mark task as done
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Mark task as not done.
+     */
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    /**
+     * Check the status of the task and return the relevant symbol
+     *
+     * @return "X" if done, otherwise " "
+     */
+    private String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        String statusIcon = "[" + getStatusIcon() + "]";
+        return String.format("%s %s", statusIcon, name);
+    }
+}
