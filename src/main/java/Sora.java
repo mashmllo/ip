f169fb8ec0b56ee1cc25bf746483c3eb2a09ee0c
mashmllo <52559284+mashmllo@users.noreply.github.com>
2026-01-15 16:47:37 +0800

    Rename chatbot from Duke to Sora
    Change the name of the chatbot from Duke to Sora and remove all references to Duke where applicable to ensure consistency.

---
+++ src/main/java/Sora.java
@@ -0,0 +1,10 @@
+public class Sora {
+    public static void main(String[] args) {
+        String logo = "'+.. + ..-. ..-.  *'.~~+'.  '  +* *''.~~+  .-. '  '''+ +   ++\n" +
+                " * .-.   ) ). ) )      . . .-.  . .'  .-. (   )  .'.-. +.'   \n" +
+                " .(   ) '-´  '-´ ' .-.  ' ( (    .-. ( ( . `-' .-.  ) ) .-. o\n" +
+                "+  `-' 'o *+'.  . (   )    `-'.'(   ) `-'.o   . ) )'-´ (   )'\n" +
+                "    o..o       +.. `-'  .''.. +  `-'     . .oo '-´ .o'. `-' o\n\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
