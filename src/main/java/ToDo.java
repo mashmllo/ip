3b280bd98539b54231e8cea35cdc9cc6758c361e
mashmllo <52559284+mashmllo@users.noreply.github.com>
2026-01-16 15:26:01 +0800

    Add ToDo class
    Introduce a ToDo class to encapsulate individual task data and behavior. This prepares the code for a cleaner task management system and improves encapsulation, making tasks easier to extend in the future.

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,25 @@
+/**
+ * ToDo task for Sora
+ *  <p>
+ *  A ToDo task are tasks without any date/time attached to it.
+ *  This class extends the base Task class and marks task type as [T]
+ */
+public class ToDo extends Task {
+
+    /**
+     * Constructor of the new ToDo task
+     * @param name Name of the ToDo task
+     */
+    public ToDo(String name) {
+        super(name);
+    }
+
+    /**
+     * Return the string representation of the ToDo task.
+     * @return formatted string e.g. "[T][X] read book"
+     */
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
