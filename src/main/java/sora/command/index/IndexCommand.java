e61b5d1770d64abf33eb3c469bc14bf3531c08c5
mashmllo <52559284+mashmllo@users.noreply.github.com>
2026-01-25 17:16:29 +0800

    Standardize IndexCommand class
    The IndexCommand class did not follow the SE
    coding conventions, including naming and layout.
    
    Non-compliant exception classes reduce code
    readability and may cause inconsistencies
    across project, making maintenance harder.
    
    Let's
    * adjust layout and spacing to follow SE
    conventions

--- src/main/java/sora/command/index/IndexCommand.java
+++ src/main/java/sora/command/index/IndexCommand.java
@@ -26,6 +26,15 @@ public abstract class IndexCommand implements Command {
     }
 
     /**
+     * Returns the index of the task.
+     *
+     * @return The task index.
+     */
+    public int getIndex() {
+        return this.index;
+    }
+
+    /**
      * Executes the command by retrieving the task at the given index
      *
      * @param taskManager  The task manager class
@@ -37,8 +46,8 @@ public abstract class IndexCommand implements Command {
 
         Task task = taskManager.getTask(index);
         if (task == null) {
-            throw new InvalidFormatException("Whoops! That task does not exist." +
-                    "\nDouble-check the number and try again");
+            throw new InvalidFormatException("Whoops! That task does not exist."
+                    + "\nDouble-check the number and try again");
         }
 
         executeOnTask(taskManager, ui);
