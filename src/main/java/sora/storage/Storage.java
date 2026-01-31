e3cb3100ec06af4d1b9668a9583b75c0a7317525
mashmllo <52559284+mashmllo@users.noreply.github.com>
2026-01-31 13:45:54 +0800

    Storage: Replace if statement with switch
    Storage currently uses multiple if statements
    to handle different task types in
    loadTask() which reduces readability and
    increases complexity as the number of
    tasks grows.
    
    Using a switch statement simplifies the logic,
    making it easier to understand and maintain.
    
    Let's
    * replace if-else chains with a switch
    statement for task parsing
    * ensure all existing tasks are correctly
    handled

--- src/main/java/sora/storage/Storage.java
+++ src/main/java/sora/storage/Storage.java
@@ -172,15 +172,16 @@ public class Storage {
 
         Task task;
 
-        if (type.equals("T")) {
-            task = new ToDo(name);
-        } else if (type.equals("D")) {
+        switch (type) {
+        case "T" -> task = new ToDo(name);
+        case "D" -> {
             if (parts.length < 4 || parts[3].trim().isEmpty()) {
                 throw new InvalidFormatException("Oops! Deadline requires /by "
                         + "and name");
             }
             task = new Deadline(name, ParsedDateTime.dateTimeParser(parts[3].trim()));
-        } else if (type.equals("E")) {
+        }
+        case "E" -> {
             if (parts.length < 5
                     || parts[3].trim().isEmpty()
                     || parts[4].trim().isEmpty()) {
@@ -190,8 +191,8 @@ public class Storage {
             task = new Event(name,
                     ParsedDateTime.dateTimeParser(parts[3].trim()),
                     ParsedDateTime.dateTimeParser(parts[4].trim()));
-        } else {
-            throw new InvalidFormatException("Oops! Unknown task type");
+        }
+        default -> throw new InvalidFormatException("Oops! Unknown task type");
         }
 
         setTaskCompletionStatus(task, completedStr);
